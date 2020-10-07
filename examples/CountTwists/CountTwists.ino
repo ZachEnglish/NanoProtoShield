ae529ce2b5392756f57c4e0723f4733fb9a18c67
Zach English <englishz@gmail.com>
2020-10-06 23:00:06 -0700

    Corrected descriptive comment on CountTwists

--- examples/CountTwists/CountTwists.ino
+++ examples/CountTwists/CountTwists.ino
@@ -9,7 +9,7 @@
       rotary encoder has been turned. Clockwise makes the count go up and
       counter-clockwise makes it go down. It is stored in an 8 bit value, so
       it wraps about 0-255. The decimal places in the 7 segment indicate one
-      hundred or two hundred. E.g. 1.2. = 212 and 12. = 120
+      hundred or two hundred. E.g. 1.2. = 212 and 12. = 112
 
     TODOs (Things that are broken/not yet working/could be improved):
       None
